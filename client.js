31e41c03574a99748726573d873aaf2c028a0783
Kaushal <kaushalkr8@gmail.com>
2025-02-09 13:41:00 +0530

    first commit

---
+++ client.js
@@ -0,0 +1,8 @@
+
+
+
+let url= "redis://default:hilOSjByegXnhtFtv88NyFsc22FEgkfG@redis-12947.c305.ap-south-1-1.ec2.redns.redis-cloud.com:12947"
+const client= new Redis(url)
+
+
+module.exports=client
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const { Redis } = require("ioredis");
+const app = express();
+app.use(express.json());
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const redis = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+});
+
+redis.on("connect", () => {
+  console.log("redis connected");
+});
+
+app.get("/cache/:key", async(req, res) => {
+    const { key } = req.params;
+    try {
+      const data = await redis.get(key);
+      if (data) {
+        return res.status(200).json({ key, data });
+      }
+      return res.status(404).json({ message: "Key not found" });
+    } catch (error) {
+      return res.status(500).json({ error: "Redis error", details: error.message });
+    }
+});
+app.post("/cache",async (req, res) => {
+    const { key, value, ttl } = req.body; 
+
+    if (!key || !value) {
+      return res.status(400).json({ error: "Key and value are required" });
+    }
+  
+    try {
+      if (ttl) {
+        await redis.setex(key, ttl, value); 
+      } else {
+        await redis.set(key, value); 
+      }
+      return res.status(201).json({ message: "Cached successfully", key, value });
+    } catch (error) {
+      return res.status(500).json({ error: "Redis error", details: error.message });
+    }
+});
+app.delete("/delete", async(req, res) => {
+    const { key } = req.params;
+
+    try {
+      const deleted = await redis.del(key);
+      if (deleted) {
+        return res.status(200).json({ message: "Deleted successfully" });
+      }
+      return res.status(404).json({ error: "Key not found" });
+    } catch (error) {
+      return res.status(500).json({ error: "Redis error", details: error.message });
+    }
+});
+
+
+app.listen(3000, () => {
+  console.log("server is running on port");
+});
